3ae40d26e4440d9be3f9bb8c1d53f8cc63c18ccb
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-26 21:55:43 +0200

    added new ways of handling error

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "src/dtos/AuthDto";
 import * as argon2 from 'argon2';
 import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService{
     constructor(private prisma:PrismaService){}
     async signUp(dto:AuthDto){
+        try{
     const hashedPassword= await argon2.hash(dto.password)
         const user = await this.prisma.user.create({
                data:{ email: dto.email,
@@ -17,6 +19,14 @@ export class AuthService{
       },
         )
 return user;
+    }catch(error){
+        if(error instanceof PrismaClientKnownRequestError)
+        if(error.code=='P2002'){
+            throw new ForbiddenException(
+                "Email already registred",
+            )
+        }
+    }
 
     }
 }

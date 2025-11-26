2006c049a4d772456eb479b96d61474dbfb3d8ee
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-26 15:01:51 +0200

    added controller

--- src/Auth/auth.controllers.ts
+++ src/Auth/auth.controllers.ts
@@ -1,7 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "src/dtos/AuthDto";
 
-@Controller()
+@Controller("/auth")
+
 export class AuthController{
     constructor(private authService:AuthService){}
+    @Post("signup")
+    signUp(@Body() dto:AuthDto){
+        return this.authService.signUp(dto)
+
+    }
 }

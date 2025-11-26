2e8b0b2fa66bc5784c0c9e54b55a479072b83fcd
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-26 14:47:15 +0200

    added dtos that have class-validator

--- src/dtos/AuthDto.ts
+++ src/dtos/AuthDto.ts
@@ -1,3 +1,11 @@
+import { IsEmail, IsNotEmpty } from "class-validator"
+
 class AuthDto{
-    
+@IsEmail()
+@IsNotEmpty()   
+email:String
+@IsNotEmpty()
+password:String
+firstName:String
+lastName:String
 }

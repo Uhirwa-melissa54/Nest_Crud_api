02b41009ebd35e68273e70ed17ba11d53002482b
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-01 21:48:39 +0200

    modified the code

---
+++ src/strategy/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy as PassportJwtStrategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(PassportJwtStrategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Read JWT from "Authorization: Bearer <token>"
+      secretOrKey: 'SECRET_KEY', // Replace with env variable
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: any) {
+    // Whatever you return here becomes req.user
+    return { userId: payload.sub, username: payload.username };
+  }
+}
+
+

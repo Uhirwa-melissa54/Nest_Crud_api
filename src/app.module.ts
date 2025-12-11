4126668d87c5d45ff7d23267a8ad15378bf22eed
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-11 21:17:38 +0200

    added the Email module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { UserModule } from './user/user.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from "@nestjs/config";
+import { EmailModule } from './email/email.module';
 
 @Module({
     imports:[AuthModule, UserModule, BookmarksModule, PrismaModule,ConfigModule.forRoot({
         isGlobal:true,
-    })]
+    }), EmailModule]
 })
 export class AppModule{}

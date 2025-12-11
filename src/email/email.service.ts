8e843e4453244e4aeeafd6af83be00dad734b9fc
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-11 21:46:47 +0200

    added send emial

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import * as nodemailer from 'nodemailer';
+import { Transporter } from 'nodemailer';
+
 
 @Injectable()
 export class EmailService{
     private transporter;
     constructor(){
-//what can i not do like  this: private transporter: nodemailer like i did before for prisma service
         this.transporter=nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 587,
       secure: false,
       auth: { user: 'uhirwamelissa@gmail.com', pass: 'uhirwashami' },
         })
+    }
+    async sendWelcomeEmail(to:string, name:string){
+        await this.transporter.sendMail({
+      from: '"NeverKnow" <uhirwamelissa@gmail.com>',
+      to,
+      subject: 'Welcome!',
+      text: `Hi ${name}, thanks for registering!`,
+      html: `<b>Hi ${name}</b>, thanks for registering!`,
+        })
+
     }
 }
